056149d40a428714d81aa08a3daa831bf3c40607
Aritra <aritrabal2@gmail.com>
2024-09-19 11:40:28 +0200

    Changes here and there

--- helpers/path_setter.py
+++ helpers/path_setter.py
@@ -15,17 +15,22 @@ def getIndex(which:str='particle',feat:str=None)->int:
 
 
 path_dict:dict[str:str]={'QCD_train':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts/train/',
-           'QCD_test':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts/test/',
-           'QAE_save':'/work/abal/qae_hep/saved_models/',
-           'QCD_lib':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts/library/',
-           'QAE_dump':'/ceph/abal/QML/dumps/',
-           'grav_2p5_narrow':'/storage/9/abal/CASE/delphes/grav_2p5_na/',
-           'grav_1p5_narrow':'/storage/9/abal/CASE/delphes/grav_1p5_na/',
-           'grav_3p5_narrow':'/storage/9/abal/CASE/delphes/grav_3p5_na/',
-            'grav_4p5_narrow':'/storage/9/abal/CASE/delphes/grav_4p5_na/',
-           'grav_3p5_broad':'/storage/9/abal/CASE/delphes/grav_3p5_br/',
-           'grav_2p5_broad':'/storage/9/abal/CASE/delphes/grav_2p5_br/',
-           'grav_1p5_broad':'/storage/9/abal/CASE/delphes/grav_1p5_br/',
+        'QCD_test':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts/test/',
+        'QCD_SR':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_signalregion_parts/',
+        'QAE_save':'/work/abal/qae_hep/saved_models/',
+        'QCD_lib':'/storage/9/abal/CASE/delphes/qcd_sqrtshatTeV_13TeV_PU40_NEW_EXT_sideband_parts/library/',
+        'QAE_dump':'/ceph/abal/QML/dumps/',
+        'grav_1p5_narrow':'/storage/9/abal/CASE/delphes/grav_1p5_na/',
+        'grav_2p5_narrow':'/storage/9/abal/CASE/delphes/grav_2p5_na/',
+        'grav_3p5_narrow':'/storage/9/abal/CASE/delphes/grav_3p5_na/',
+        'grav_4p5_narrow':'/storage/9/abal/CASE/delphes/grav_4p5_na/',
+        'grav_3p5_broad':'/storage/9/abal/CASE/delphes/grav_3p5_br/',
+        'grav_2p5_broad':'/storage/9/abal/CASE/delphes/grav_2p5_br/',
+        'grav_1p5_broad':'/storage/9/abal/CASE/delphes/grav_1p5_br/',
+        'AtoHZ_1p5':'/storage/9/abal/CASE/delphes/AtoHZ_1p5/',
+        'AtoHZ_2p5':'/storage/9/abal/CASE/delphes/AtoHZ_2p5/',
+        'AtoHZ_3p5':'/storage/9/abal/CASE/delphes/AtoHZ_3p5/',
+        'AtoHZ_4p5':'/storage/9/abal/CASE/delphes/AtoHZ_4p5/',
            }
 
 eventFeatureNames:list[str]=['mJJ', 'j1Pt', 'j1Eta', 'j1Phi', 'j1M', 'j1E', 'j2Pt',
@@ -33,3 +38,6 @@ eventFeatureNames:list[str]=['mJJ', 'j1Pt', 'j1Eta', 'j1Phi', 'j1M', 'j1E', 'j2P
 
 particleFeatureNames:list[str]=['eta', 'phi', 'pt']
 
+labels={'grav_1p5_narrow':'$M_{grav}=1.5$ TeV','grav_2p5_narrow':'$M_{grav}=2.5$ TeV','grav_3p5_narrow':'$M_{grav}=3.5$ TeV'\
+        ,'grav_4p5_narrow':'$M_{grav}=4.5$ TeV','AtoHZ_1p5':'$M_{A}=1.5$ TeV','AtoHZ_2p5':'$M_{A}=2.5$ TeV',\
+            'AtoHZ_3p5':'$M_{A}=3.5$ TeV','AtoHZ_4p5':'$M_{A}=4.5$ TeV'}
