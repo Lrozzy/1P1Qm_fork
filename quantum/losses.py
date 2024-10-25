ee5e4fd0fac0aa60b6c3e0cb7a24f7223f5a7f5d
Aritra <aritrabal2@gmail.com>
2024-10-25 12:52:25 +0200

    Added so-called quantum loss that sums up probabilities of |0> state

--- quantum/losses.py
+++ quantum/losses.py
@@ -25,3 +25,30 @@ def batch_semi_classical_cost(weights,inputs=None,quantum_cost=None,return_fid=F
     cost=1-fid
     batched_average_cost=100.*(np.array(cost,requires_grad=True).mean())#np.array([-quantum_cost(weights,item) for item in inputs]).mean()
     return batched_average_cost
+
+def batch_quantum_cost(weights,inputs=None,quantum_cost=None,return_fid=False):
+    probs=np.array(quantum_cost(weights,inputs))[...,0] # the first element is the probability of the |0> state, which is a function of the fidelity
+    # Prob(0) = 0.5*(1+fid**2)  --> We need to invert this
+    fid = np.sqrt(2*probs-1)
+    cost=1-fid
+    if return_fid:
+        return np.mean(100.*cost),np.mean(100.*fid)
+    # the minus sign is to maximize the fidelity between the trash and reference states - which implies that the output and input states are close to each other
+    #fid=np.sqrt(fid)
+    
+    batched_average_cost=100.*np.mean(cost)#np.array([-quantum_cost(weights,item) for item in inputs]).mean()
+    return batched_average_cost
+
+
+def quantum_cost(weights,inputs=None,quantum_cost=None,return_fid=False):
+    probs=np.array(quantum_cost(weights,inputs))[...,0] # the first element is the probability of the |0> state, which is a function of the fidelity
+    # Prob(0) = 0.5*(1+fid**2)  --> We need to invert this
+    fid = np.sqrt(2*probs-1)
+    cost=1-fid
+    if return_fid:
+        return np.mean(100.*cost,axis=0,requires_grad=False),np.mean(100.*fid,axis=0,requires_grad=False)
+    # the minus sign is to maximize the fidelity between the trash and reference states - which implies that the output and input states are close to each other
+    #fid=np.sqrt(fid)
+    
+    batched_average_cost=100.*np.mean(cost,axis=0,requires_grad=False)#np.array([-quantum_cost(weights,item) for item in inputs]).mean()
+    return batched_average_cost
